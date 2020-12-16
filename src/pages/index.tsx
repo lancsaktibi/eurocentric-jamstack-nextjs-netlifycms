e4de2468529b34f5ef064e2dc01efb098baa9ac1
Tibor Lancsak <tibor.lancsak@pa-ats.com>
2020-12-16 16:02:04 +0100

    Index updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Welcome to Eurocentric<span className="fancy">.</span>
           </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <span className="handle">@eurocentric</span>
+          <h2>A preview of selected Web Technologies.</h2>
           <SocialList />
         </div>
       </div>
